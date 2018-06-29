9f421331163665c84e0a586a4514e00b65a4e1dc
AlexandrXXL <Alexandr539XXL@mail.ru>
2018-06-29 16:54:31 +0300

    Теперь и с тестами

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -8,13 +8,33 @@ import java.awt.*;
 
 public class MineSweeper {
     public static void main(String[] args) {
+        if (args.length != 3 || Integer.valueOf(args[0]) <= 0 || Integer.valueOf(args[1]) <= 0
+                || Integer.valueOf(args[2]) <= 0) {
+            System.out.println("неверный аргумент");
+            return;
+        }
+        int w = Integer.valueOf(args[0]);
+        if (w > 25) {
+            System.out.println("слишком широкое поле");
+            return;
+        }
+        int h = Integer.valueOf(args[1]);
+        if (h > 11) {
+            System.out.println("Слишком высокое поле");
+            return;
+        }
+        int m = Integer.valueOf(args[2]);
+        if (m >= w * h) {
+            System.out.println("неправильно задано количество мин");
+            return;
+        }
         JFrame frame = new JFrame();
         GameResultDialog failDialog =
                 new GameResultDialog (frame, "<html><h1><i>Вы проиграли!!!</h1></html>");
         GameResultDialog successDialog =
                 new GameResultDialog (frame, "<html><h1><i>Вы выиграли!!!</h1></html>");
-        MineSweeperGame game = new MineSweeperGame (15, 10,  40, 10,
-                30,
+        MineSweeperGame game = new MineSweeperGame (w, h,  40, 10,
+                m,
                 successDialog, failDialog);
         game.clear ();
         frame.setSize (game.getFieldWidth (), game.getFieldHeight ());
